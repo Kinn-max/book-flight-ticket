3d426e43b526599526b069ff93703d1289e6825a
Kinn-max <kienhien200418@gmail.com>
2024-11-16 01:14:18 +0700

    config cors

---
+++ src/main/java/com/bookflight/ticket/configuration/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.bookflight.ticket.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")// Cấu hình cho tất cả các endpoint fe
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
