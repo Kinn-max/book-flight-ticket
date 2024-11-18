0d1282681cb9b156d36834185be5afe9cefec970
Kinn-max <kienhien200418@gmail.com>
2024-11-18 15:44:57 +0700

    create folder

--- backend/src/main/java/com/bookflight/ticket/configuration/CorsConfig.java
+++ backend/src/main/java/com/bookflight/ticket/configuration/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")// Cấu hình cho tất cả các endpoint fe
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
